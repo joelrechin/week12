f5b093b9880492f986aa5eb400e1089cbf6b6917
jolisko <joelrechin@gmail.com>
2021-11-21 02:14:07 +0000

    final add

--- db.tf
+++ db.tf
@@ -1,3 +1,12 @@
+resource "aws_db_subnet_group" "week12-sng" {
+  name       = "week12-sng"
+  subnet_ids = [aws_subnet.week12-pri-a.id, aws_subnet.week12-pri-b.id]
+
+  tags = {
+    Name = "week12-sng"
+  }
+}
+
 resource "aws_security_group" "week12-rds-sg" {
   name        = "week12-rds-sg"
   description = "Allow MySQL inbound traffic"
@@ -37,4 +46,24 @@ resource "aws_security_group" "week12-rds-sg" {
     Name = "week12-rds-sg"
   }
 }
+
+resource "aws_rds_cluster" "week12-rds" {
+  cluster_identifier     = "week12-rds"
+  database_name          = "week12_rds"
+  engine                 = "aurora-mysql"
+  engine_mode            = "serverless"
+  master_username        = "admin"
+  master_password        = "secret123"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.week12-sng.id
+  vpc_security_group_ids = [aws_db_subnet_group.week12-sng.id]
+
+  scaling_configuration {
+    auto_pause              = true
+    max_capacity            = 2
+    min_capacity            = 1
+    seconds_until_auto_pause = 300
+  }
+}
+
 
